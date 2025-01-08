caec9aef0671c0258a460019d77bc8eef1cc68d2
Venkat <venkata@venkatamutyala.com>
2025-01-08 22:31:49 +0000

    Feat/adding image cache (#28)
    * fix: remove quotes for public key
    
    * fix: parsing of existing VM when vm is stopped
    
    * fix: schemas.py with correct examples
    
    * feat: updating script sto support image cache
    
    * fix: poll for images every 2 minutes
    
    * fix: point to next release
    
    * fix: point to next release v0.26.0 of dev utilities

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Dict
 
 class ExistingVm(BaseModel):
-    dom_id: int = Field(...,example = 1)
+    dom_id: str = Field(...,example = "1")
     name: str = Field(...,example = 'dinosaur-cat')
     region_name: str = Field(...,example = 'andromeda')
     state: str = Field(...,example = 'running')
@@ -12,9 +12,9 @@ class Vm(BaseModel):
     vm_name: str = Field(...,example = 'dinosaur-cat')
     tags: dict = Field(...,example = {"owner": "john-doe"})
     user_data: str = Field(...,example = 'I2Nsb3VkLWNvbmZpZwpydW5jbWQ6CiAgLSBbJ3Bhc3N3ZCcsICctZCcsICdkZWJpYW4nXQo=')
-    image: str = Field(...,example = 'v0.72.0-rc4')
+    image: str = Field(...,example = 'v0.76.0')
     region_name: str = Field(...,example = 'andromeda')
-    instance_type: str = Field(...,example = 'small')
+    instance_type: str = Field(...,example = 'basic')
 
 class VmMeta(BaseModel):
     vm_name: str = Field(...,example = 'dinosaur-cat')
