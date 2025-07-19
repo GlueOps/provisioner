fcd2d1d960268ba145cfc39f62c606f308ae873b
Nicholas Archuletta <archulettanicholas@gmail.com>
2025-07-19 01:17:15 +0000

    fix: comment out code for testing

--- app/main.py
+++ app/main.py
@@ -180,9 +180,9 @@ async def import_vm(vm: VmImport, api_key: str = Depends(get_api_key)):
                 vm_id
             )
 
-        command = f"virsh desc {vm.vm_name} --config --new-desc '{b64.encode_string(json.dumps(vm.tags))}'"
-        cfg = regions.get_server_config(vm.region_name, REGIONS)
-        ssh.execute_ssh_command(cfg.host, cfg.user, cfg.port, command)
+        # command = f"virsh desc {vm.vm_name} --config --new-desc '{b64.encode_string(json.dumps(vm.tags))}'"
+        # cfg = regions.get_server_config(vm.region_name, REGIONS)
+        # ssh.execute_ssh_command(cfg.host, cfg.user, cfg.port, command)
 
     except Exception as e:
         logger.error(f"vm import failed: {e.stderr}")
